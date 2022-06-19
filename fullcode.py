92a03b884b6525a8c68445b1976c447b35522513
GedalyahRookout <100343693+GedalyahRookout@users.noreply.github.com>
2022-06-19 21:44:58 +0300

    Create fullcode.py

---
+++ fullcode.py
@@ -0,0 +1,22 @@
+def inorder() -> object:
+   for key, value in sorted(counts.items(), key=itemgetter(1), reverse=True):
+       print(key,':', value)
+
+file = open("debuggingwiki.txt", "r")
+read_data = file.read()
+per_word = read_data.split()
+
+file = open("debuggingwiki.txt")
+counts = dict()
+for line in file:
+   words = line.split()
+   for word in words:
+       if word in counts:
+           counts[word] += 1
+       else:
+           counts[word] = 1
+
+totalcount()
+wordsbynum()
+print('In order:')
+inorder()

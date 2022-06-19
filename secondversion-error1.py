858930df864c23f2ff3af3a2af74d7023a950463
GedalyahRookout <100343693+GedalyahRookout@users.noreply.github.com>
2022-06-19 21:50:21 +0300

    Create secondversion-error1.py

---
+++ secondversion-error1.py
@@ -0,0 +1,27 @@
+def totalcount():
+print('Total Words:', len(per_word))
+
+def wordsbynum():
+print('Word Frequency:', counts)
+
+file = open("debuggingwiki.txt", "r")
+read_data = file.read()
+per_word = read_data.split()
+
+file = open("debuggingwiki.txt")
+counts = dict()
+for line in file:
+words = line.split()
+for word in words:
+if word in counts:
+counts[word] += 1
+else:
+counts[word] = 1
+
+totalcount()
+wordsbynum()
+
+#First try with new code additions
+def inorder():
+   freqsort = sort.counts()
+   print(freqsort)
